0a99567319fc9de4e75fc21b245001620dd9947e
pratikpatil121 <pratikphotograph@gmail.com>
2023-12-29 23:31:54 +0530

    Inheritance

---
+++ Inheritance/Destructor2.py
@@ -0,0 +1,24 @@
+#Destructor
+
+class Parent:
+	z=30
+
+	def __init__(self):
+		print("In Constructor")
+		self.x=10
+		self.y=20
+
+	def dispData(self):
+		print(self.x)
+		print(self.y)
+
+	@classmethod
+	def dispParent(cls):
+		print(cls.z)
+
+	def __del__(self):
+		print("In Destructor")
+
+obj1=Parent()
+obj1=Parent()
+print("Code End")
